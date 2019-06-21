d5b74ad1f5544133787c0557b21864658eb62fd0
Yuriy Markov <peacefullatom@gmail.com>
2019-06-22 00:43:15 +0500

    feat: added links to tools used in the project to the footer. scss clean-up.

--- src/base/Footer.tsx
+++ src/base/Footer.tsx
@@ -1,10 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { footerLinks } from './const';
 import './Footer.scss';
+import { IFooter } from './types';
+
+const FooterLink: React.FC<IFooter> = props => {
+  return (
+    <a href={props.link} target='_blank' rel='noopener noreferrer'>
+      <FontAwesomeIcon icon={props.icon} className='mx-1 footer__icon' />
+    </a>
+  );
+};
 
 const Footer: React.FC = () => {
   const year = new Date().getFullYear();
 
-  return <div className='footer mt-auto py-3 pr-3 d-flex justify-content-end flex-shrink-0'>&copy; {year} Yuriy Markov</div>;
+  return (
+    <div className='footer mt-auto py-3 px-3 d-flex justify-content-between flex-shrink-0'>
+      <div>
+        {footerLinks.map((link, key) => (
+          <FooterLink key={key} {...link} />
+        ))}
+      </div>
+      <div>&copy; {year} Yuriy Markov</div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/base/const.ts
+++ src/base/const.ts
@@ -1,8 +1,10 @@
+import { faBootstrap, faGitSquare, faReact, faSass, faYarn } from '@fortawesome/free-brands-svg-icons';
 import About from 'page/about/About';
 import Contact from 'page/contact/Contact';
 import Gallery from 'page/gallery/Gallery';
 import Intro from 'page/intro/Intro';
 import { ILink } from 'types';
+import { IFooter } from './types';
 
 export const defaultLink: ILink = { path: '/', label: 'Intro', component: Intro };
 
@@ -10,4 +12,12 @@ export const baseLinks: ILink[] = [
   { path: '/gallery', label: 'Gallery', component: Gallery },
   { path: '/about', label: 'About', component: About },
   { path: '/contact', label: 'Contact', component: Contact }
+];
+
+export const footerLinks: IFooter[] = [
+  { icon: faReact, link: 'https://reactjs.org/' },
+  { icon: faBootstrap, link: 'https://getbootstrap.com/' },
+  { icon: faGitSquare, link: 'https://git-scm.com/' },
+  { icon: faSass, link: 'https://sass-lang.com/' },
+  { icon: faYarn, link: 'https://yarnpkg.com/' }
 ];
